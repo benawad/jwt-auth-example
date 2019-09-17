b4a61b6bb997e18c89209667d316616513b81020
Ben Awad <benawad97@gmail.com>
2019-09-17 11:23:25 -0500

    persist refreshes

---
+++ web/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { Routes } from "./Routes";
+import { setAccessToken } from "./accessToken";
+
+interface Props {}
+
+export const App: React.FC<Props> = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/refresh_token", {
+      method: "POST",
+      credentials: "include"
+    }).then(async x => {
+      const { accessToken } = await x.json();
+      setAccessToken(accessToken);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  return <Routes />;
+};
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { Routes } from "./Routes";
 import { getAccessToken } from "./accessToken";
+import { App } from "./App";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -22,7 +22,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Routes />
+    <App />
   </ApolloProvider>,
   document.getElementById("root")
 );
