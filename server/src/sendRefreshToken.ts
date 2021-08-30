5ff0a88f27d207aa744a8400c613d7eae970038a
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2021-08-31 00:36:39 +0530

    added cookies options

--- server/src/sendRefreshToken.ts
+++ server/src/sendRefreshToken.ts
@@ -3,6 +3,7 @@ import { Response } from "express";
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("jid", token, {
     httpOnly: true,
-    path: "/refresh_token"
+    secure: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days token expiration time
   });
 };
