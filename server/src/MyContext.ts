df45892c93209cec6d8547a1a7c33b1ab1f39666
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2021-08-31 00:37:49 +0530

    added auth middleware for accessToken and refreshToken

--- server/src/MyContext.ts
+++ server/src/MyContext.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
+import { User } from "./entity/User";
 
 export interface MyContext {
   req: Request;
   res: Response;
-  payload?: { userId: string };
+  payload?: User;
 }
--- server/src/isAuth.ts
+++ server/src/isAuth.ts
@@ -1,24 +1,45 @@
 import { MiddlewareFn } from "type-graphql";
 import { verify } from "jsonwebtoken";
 import { MyContext } from "./MyContext";
+import { createAccessToken } from "./auth";
+import { User } from "./entity/User";
 
 // bearer 102930ajslkdaoq01
 
-export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
-  const authorization = context.req.headers["authorization"];
+export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
+  if (context.req.cookies.jid) {
+    const authorization = context.req.headers["authorization"];
+    if (!authorization) {
+      context.res.clearCookie("jid");
+      throw new Error("not authenticated");
+    }
 
-  if (!authorization) {
-    throw new Error("not authenticated");
-  }
-
-  try {
-    const token = authorization.split(" ")[1];
-    const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
-    context.payload = payload as any;
-  } catch (err) {
-    console.log(err);
-    throw new Error("not authenticated");
-  }
-
-  return next();
+    try {
+      const token = authorization.split(" ")[1];
+      const { userId } = verify(token, process.env.ACCESS_TOKEN_SECRET!) as { userId: number };
+      const user = await User.findOne({ where: { id: userId } });
+      if (!user) {
+        context.res.clearCookie("jid");
+        throw new Error("not authenticated");
+      }
+      context.payload = user;
+    } catch (err) {
+      try {
+        const { userId, tokenVersion } = verify(context.req.cookies.jid, process.env.REFRESH_TOKEN_SECRET!) as { userId: number; tokenVersion: number; };
+        const user = await User.findOne({ where: { id: userId } });
+        if (!user || user.tokenVersion !== tokenVersion) {
+          context.res.clearCookie("jid");
+          throw new Error("not authenticated");
+        }
+        context.res.setHeader("access-token", createAccessToken(user));
+        context.payload = user;
+      } catch (error) {
+        console.log(error);
+        context.res.clearCookie("jid");
+        throw new Error("not authenticated");
+      }
+    }
+    return next();
+  };
+  throw new Error("not authenticated");
 };
